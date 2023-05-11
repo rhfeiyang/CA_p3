24a2510055ef24e96edffd9df96b7d3cddad4766
breadteeth <zhangxx5@shanghaitech.edu.cn>
2023-05-11 22:40:11 +0800

    util.c

--- utils.c
+++ utils.c
@@ -87,7 +87,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
   float w0 = params->density * 4.f / 9.f;
   float w1 = params->density      / 9.f;
   float w2 = params->density      / 36.f;
-
+  #pragma omp parallel for schedule(static)
   for (int jj = 0; jj < params->ny; jj++)
   {
     for (int ii = 0; ii < params->nx; ii++)
@@ -108,6 +108,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
   }
 
   /* first set all cells in obstacle array to zero */
+  #pragma omp parallel for schedule(static)
   for (int jj = 0; jj < params->ny; jj++)
   {
     for (int ii = 0; ii < params->nx; ii++)
@@ -194,6 +195,7 @@ int write_state(char* filename, const t_param params, t_speed* cells, int* obsta
   }
 
   /* loop on grid to calculate the velocity of each cell */
+  #pragma omp parallel for schedule(static)
   for (int jj = 0; jj < params.ny; jj++)
   {
     for (int ii = 0; ii < params.nx; ii++)
