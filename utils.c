8cad03cef293b80508adee2e15b014cef6c06e61
breadteeth <zhangxx5@shanghaitech.edu.cn>
2023-05-13 10:40:16 +0800

    omp for util

--- utils.c
+++ utils.c
@@ -87,7 +87,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
     float w0 = params->density * 4.f / 9.f;
     float w1 = params->density      / 9.f;
     float w2 = params->density      / 36.f;
-
+    #pragma omp parallel for schedule(static) collapse(2)
     for (int jj = 0; jj < params->ny; jj++)
     {
         for (int ii = 0; ii < params->nx; ii++)
@@ -109,7 +109,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
     }
 
     /* first set all cells in obstacle array to zero */
-
+    #pragma omp parallel for schedule(static) collapse(2)
     for (int jj = 0; jj < params->ny; jj++)
     {
         for (int ii = 0; ii < params->nx; ii++)
